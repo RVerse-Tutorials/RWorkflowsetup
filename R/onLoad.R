7110ebd787539f026d18761e9f67c6fcfb9a3aae
EEH <eli.holmes@noaa.gov>
2018-09-04 19:48:42 -0700

    move ggplot2 and rmarkdown install to onLoad

---
+++ R/onLoad.R
@@ -0,0 +1,4 @@
+.onLoad <- function(libname, pkgname){
+  install.packages("ggplot2", repos = "https://cran.revolutionanalytics.com/")
+  install.packages("rmarkdown", repos = "https://cran.revolutionanalytics.com/")
+}
